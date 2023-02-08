0814a6e549e177b743fd496920447eade7a6c3a0
ppopow <pawel.popow@gmail.com>
2023-02-08 17:26:18 +0000

    two commit

--- games/tic_tac_toe.py
+++ games/tic_tac_toe.py
@@ -120,5 +120,57 @@ class Board(object):
         self.draw_text(self.surface, score, center=(i, i), color=(255, 26, 26))
 
 
+class TicTacToeGame(object):
+    """
+        Łączy wszystkie elementy gry w całość.
+    """
+
+    def __init__(self, width, ai_turn=False):
+        """
+            Przygotowanie ustawień gry
+            :param width: szerokość planszy mierzona w pikselach
+        """
+        pygame.init()
+        # zegar którego użyjemy do kontrolowania szybkości rysowania
+        # kolejnych klatek gry
+        self.fps_clock = pygame.time.Clock()
+
+        self.board = Board(width)
+        self.ai = Ai(self.board)
+        self.ai_turn = ai_turn
+
+    def run(self):
+        """
+            Główna pętla gry
+        """
+        while not self.handle_events():
+            # działaj w pętli do momentu otrzymania sygnału do wyjścia
+            self.board.draw()
+            if self.ai_turn:
+                self.ai.make_turn()
+                self.ai_turn = False
+            self.fps_clock.tick(15)
+
+    def handle_events(self):
+        """
+            Obsługa zdarzeń systemowych, tutaj zinterpretujemy np. ruchy myszką
+
+            :return True jeżeli pygame przekazał zdarzenie wyjścia z gry
+        """
+        for event in pygame.event.get():
+            if event.type == pygame.locals.QUIT:
+                pygame.quit()
+                return True
+
+            if event.type == pygame.locals.MOUSEBUTTONDOWN:
+                if self.ai_turn:
+                    # jeśli jeszcze trwa ruch komputera to ignorujemy zdarzenia
+                    continue
+                # pobierz aktualną pozycję kursora na planszy mierzoną w pikselach
+            x, y = pygame.mouse.get_pos()
+            self.board.player_move()
+            self.ai_turn = True
+
+
 if __name__ == '__main__':
     pass
